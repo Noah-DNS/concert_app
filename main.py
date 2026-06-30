c24189f7889dc162662e04b8f8f73bafa3ae69f8
Noah DENIS <Noah@MacBook-Air-2.local>
2026-06-30 12:21:17 +0200

    ajout page import excel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request, UploadFile, File
+from openpyxl import load_workbook
 import database
 
 app = FastAPI()
@@ -31,7 +33,35 @@ def scan_page(request: Request):
         request=request,
         name="scan.html"
     )
+@app.get("/import")
+async def import_page(request: Request):
+    return templates.TemplateResponse(
+        "import.html",
+        {"request": request}
+    )
 
+
+@app.post("/import")
+async def import_excel(file: UploadFile = File(...)):
+
+    import openpyxl
+
+    workbook = openpyxl.load_workbook(file.file)
+
+    sheet = workbook.active
+
+
+    for row in sheet.iter_rows(values_only=True):
+
+        numero = str(row[0])
+        nom = row[1] if len(row) > 1 else ""
+
+        database.add_ticket(numero, nom)
+
+
+    return {
+        "message": "Tickets importés"
+    }
 
 @app.get("/scan/{ticket_id}")
 def scan(ticket_id: str):
